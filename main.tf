ab5c17b3375c38cdc7c0ad1001fe6107392d77f9
tzquocuong918 <72614483+tzquocuong918@users.noreply.github.com>
2024-03-14 14:33:00 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,32 +2,55 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.55.0"
+      version = "~> 5.0"
     }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-southeast-1"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region     = "ap-southeast-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
-module "vpc" {
-  source         = "./vpc"
-  vpc_cidr_block = "10.0.0.0/16"
-  Private_subnet = ["10.0.10.0/24", "10.0.20.0/24"]
-  Public_subnet  = ["10.0.1.0/24", "10.0.2.0/24"]
-  az             = ["ap-southeast-1a", "ap-southheast-1c"]
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
-module "ec2" {
-  source         = "./EC2"
-  vpc            = module.vpc
-  sg_public      = module.vpc.sg_public
-  subnet_public  = module.vpc.subnet_public
-  sg_private     = module.vpc.sg_private
-  subnet_private = module.vpc.subnet_private
-  az             = ["ap-southeast-1a", "ap-southeast-1c"]
 
+# Create a subnet
+resource "aws_subnet" "example" {
+  vpc_id     = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
+}
 
+# Create a security group
+resource "aws_security_group" "example" {
+  vpc_id = aws_vpc.example.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create an EC2 instance in the VPC
+resource "aws_instance" "example" {
+  ami           = "ami-0095e05a7e9619643"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.example.id
+  vpc_security_group_ids = [aws_security_group.example.id]
+  tags = {
+    Name = "Morpheus-Intance"
+  }
 }
